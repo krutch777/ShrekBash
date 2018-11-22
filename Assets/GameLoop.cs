b32ac9a4c36c6b67341b89c800bb1fa9662f3252
agusterodin <jeffrosen1092@gmail.com>
2018-11-22 03:17:39 -0500

    End of Night Pre Thanksgiving

---
+++ Assets/GameLoop.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoop : MonoBehaviour {
+
+
+	public GameObject crate;
+	public int maxCrates = 10;
+	public int currentCrates;
+	ArrayList crates;
+
+	// Use this for initialization
+	void Start() {
+		crates = new ArrayList();
+		GenerateCrates();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		GenerateCrates();
+		//if (Input.GetKeyDown("space")) {
+		//	var position = RandomValue();
+		//	print(position);
+		//	crates.Add(Instantiate(crate, position, Quaternion.identity));
+		//}
+
+
+	}
+
+	void GenerateCrates() {
+		while (currentCrates < maxCrates) {
+			var position = RandomValue();
+			print(position);
+			crates.Add(Instantiate(crate, position, Quaternion.identity));
+			currentCrates++;
+		}
+	}
+
+		Vector3 RandomValue() {
+		Start:
+			Vector3 val;
+			while (true) {
+				val = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+				foreach (GameObject g in crates) {
+					if (Vector3.Distance(g.transform.position, val) < 1) goto Start;
+
+				}
+				goto Outer;
+			}
+		Outer:
+			return val;
+		}
+}
+
--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -4,52 +4,86 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
+	GameObject holding;
 	Vector3 currentDirection;
 	static Animator anim;
-	float speedConstant = 1f;
-	float rot =0;
-	// Use this for initialization
-	void Start () {
+	public float moveSpeed = 10;
+	private float moveHorizontal;
+	private float moveVertical;
+
+	void Start() {
 		currentDirection = new Vector3(0, 0, 0);
 		anim = GetComponent<Animator>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		float speed = speedConstant * Time.deltaTime;
+
+	void Update() {
 		float moveHorizontal = Input.GetAxisRaw("Horizontal");
 		float moveVertical = Input.GetAxisRaw("Vertical");
-
-
-		if (Input.GetKey(KeyCode.LeftArrow)) {
-			transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * speed, transform.position.y, transform.position.z + Input.GetAxis("Vertical") * speed);
-			currentDirection = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
+		if (Mathf.Abs(moveVertical) < .15) {
+			moveVertical = 0;
 		}
-		if (Input.GetKey(KeyCode.RightArrow)) {
-			transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * speed, transform.position.y, transform.position.z + Input.GetAxis("Vertical") * speed);
-			currentDirection = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-		}
-		if (Input.GetKey(KeyCode.UpArrow)) {
-			transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * speed, transform.position.y, transform.position.z + Input.GetAxis("Vertical") * speed);
-			currentDirection = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-		}
-		if (Input.GetKey(KeyCode.DownArrow)) {
-			transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * speed, transform.position.y, transform.position.z + Input.GetAxis("Vertical") * speed);
-			currentDirection = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		if (Mathf.Abs(moveHorizontal) < .15) {
+			moveHorizontal = 0;
 		}
 
-		if (Input.GetKey(KeyCode.Space)) {
-			anim.SetTrigger("jump");
-			GetComponent<Rigidbody2D>().AddForce(Vector2.up * speed);
+		print(transform.eulerAngles);
+
+		keyboardControls();
+		print(moveHorizontal + "   " + moveVertical);
+
+		if (moveHorizontal != 0 || moveVertical != 0) {
+			anim.SetTrigger("run");
+			transform.position = new Vector3(transform.position.x + moveHorizontal * moveSpeed * Time.deltaTime, transform.position.y, transform.position.z - (moveVertical * moveSpeed * Time.deltaTime));
+			transform.eulerAngles = new Vector3(0, (Mathf.Atan2(moveHorizontal, -moveVertical) * (180 / Mathf.PI)), 0);
 		}
-		//print(Input.GetAxis("Horizontal"));
-		transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(currentDirection), 15 * Time.deltaTime); // interpolate for smooth camera motion
-		transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * speed, transform.position.y, transform.position.z - (Input.GetAxis("Vertical") * speed));
-		currentDirection = new Vector3(moveHorizontal, 0.0f, -moveVertical);
-		transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(currentDirection), 15 * Time.deltaTime); // interpolate for smooth camera motion
+		else {
+			anim.SetTrigger("idle");
+		}
+
+		if (Input.GetKey(KeyCode.B)) {
+			if (holding != null) {
+				Rigidbody rb = holding.GetComponent<Rigidbody>();
+				rb.isKinematic = false;
+				Vector3 dir = new Vector3(100, 0, 0);
+				dir.Normalize();
+				rb.AddForce(transform.forward * 600);
+				//print(transform.eulerAngles.Normalize * );
+				holding = null;
+
+			}
+		}
+		if (holding != null) {
+			holding.transform.position = new Vector3(transform.position.x, 1.5f, transform.position.z);
+		}
 
 	}
+
+	void keyboardControls() {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			moveHorizontal = -1f;
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			moveHorizontal = 1f;
+		}
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			moveVertical = 1f;
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			moveVertical = -1f;
+		}
+		if (Input.GetKey(KeyCode.Space)) {
+			anim.SetTrigger("jump");
+			GetComponent<Rigidbody>().AddForce(Vector2.up * moveSpeed);
+		}
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		print("Chan");
+		if (other.tag == "Crate") {
+			holding = other.gameObject;
+
+		}
+	}
+
+
 }
